04c2ab1006e74a18b74e2881dfb06ad4f09ba1bf
omidhap <ohajabdolpoor@gmail.com>
2018-07-14 15:18:39 +0430

    RssData now save all needed information

--- RSSReader/src/main/java/ir/sahab/nimbo2/RssData.java
+++ RSSReader/src/main/java/ir/sahab/nimbo2/RssData.java
@@ -1,13 +1,67 @@
 package ir.sahab.nimbo2;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.net.URL;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class RssData {
 
-  public ArrayList<HashMap<String,String>> getRssData(String link) {
-    ArrayList<HashMap<String,String>> rssDataHMap = new ArrayList<HashMap<String,String>>();
-    //todo
+  public ArrayList<HashMap<String, String>> getRssData(String link) {
+    ArrayList<HashMap<String, String>> rssDataHMap = new ArrayList<HashMap<String, String>>();
+    String url = link;
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    DocumentBuilder db = null;
+    try {
+      db = dbf.newDocumentBuilder();
+    } catch (ParserConfigurationException e) {
+      e.printStackTrace();
+    }
+    Document doc = null;
+    try {
+      doc = db.parse(new URL(url).openStream());
+    } catch (SAXException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    for (int i = 0; i < doc.getElementsByTagName("item").getLength(); i++) {
+      rssDataHMap.add(new HashMap<String, String>());
+      for (int j = 0;
+           j < doc.getElementsByTagName("item").item(i).getChildNodes().getLength();
+           j++) {
+        if (doc.getElementsByTagName("item")
+                .item(i)
+                .getChildNodes()
+                .item(j)
+                .toString()
+                .contains("title")) {
+          rssDataHMap.get(i).put("title" , doc.getElementsByTagName("item").item(i).getChildNodes().item(j).getTextContent());
+        }
+        else if (doc.getElementsByTagName("item")
+                .item(i)
+                .getChildNodes()
+                .item(j)
+                .toString()
+                .contains("link")) {
+          rssDataHMap.get(i).put("link" , doc.getElementsByTagName("item").item(i).getChildNodes().item(j).getTextContent());
+        }
+        else if (doc.getElementsByTagName("item")
+                .item(i)
+                .getChildNodes()
+                .item(j)
+                .toString()
+                .contains("pubDate")) {
+          rssDataHMap.get(i).put("pubDate" , doc.getElementsByTagName("item").item(i).getChildNodes().item(j).getTextContent());
+        }
+      }
+    }
     return rssDataHMap;
   }
 }
